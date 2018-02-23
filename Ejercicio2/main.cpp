25fa66304bd41c0b0aec2d07bf0cfd0e3a21648c
SnT <santiago.narvaez@udea.edu.co>
2018-02-23 11:37:34 -0500

    Práctica #2; Números pares e Impares

---
+++ Ejercicio2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num;
+    cout<<"Escriba un numero: ";
+    cin >> num;
+    cout<<"El número que usted escribio es: "<<num<<endl;
+
+    if(num%2==0){
+        cout<<"El número "<<num<<" es par."<<endl;
+        }
+    else{
+        cout<<"El número "<<num<<" es impar."<<endl;
+        }
+    }
