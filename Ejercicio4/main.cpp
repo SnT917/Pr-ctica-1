e13d0b8af1d0e89d800f6a1d5cbab35ee7d64590
SnT <santiago.narvaezt@udea.edu.co>
2018-02-23 12:31:42 -0500

    Ejercicio #4 Números Mayores y Menores.

--- Ejercicio4/main.cpp
+++ Ejercicio4/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 using namespace std;
 
+//Ejercicio 4. Escriba un programa que pida dos números A y B e imprima en pantalla el menor.
+//Ej: si se ingresan 7 y 3 se debe imprimir:
+//El menor es 3
+
 int main(){
     int A, B;
     cout << "Ingrese el primer número: ";
-    cin >> A;
+    cin >> A; //The user put's the first number.
     cout << "Ingrese el segundo número: ";
-    cin >> B;
+    cin >> B; //The user put's the second number.
 
-    if(A>B){
+    if(A>B){ //This condition compare if the first number is bigger than the second one, if that is the case, the program will show the second number (Smaller one).
         cout <<B<<" es el número menor"<<endl;
            }
     else{
-        cout <<A<<" es el número menor"<<endl;
+        cout <<A<<" es el número menor"<<endl; //Otherwise, will show the first one.
         }
 
     return 0;
