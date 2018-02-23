18188ccd70c77edfd6acee6c5bdc02f99282aff7
SnT <santiago.narvaezt@udea.edu.co>
2018-02-23 12:23:19 -0500

    Ejercicio #4: Número Mayor y Menor (Sin comentarios).

---
+++ Ejercicio4/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, B;
+    cout << "Ingrese el primer número: ";
+    cin >> A;
+    cout << "Ingrese el segundo número: ";
+    cin >> B;
+
+    if(A>B){
+        cout <<B<<" es el número menor"<<endl;
+           }
+    else{
+        cout <<A<<" es el número menor"<<endl;
+        }
+
+    return 0;
+}
