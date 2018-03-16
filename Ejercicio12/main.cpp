a253c8dd952ddf806eb9fbb5552b348c79611f35
SnT917 <santiago.narvaezt@udea.edu.co>
2018-03-16 09:49:50 -0500

    Ejercicio #12: Potencias de n.

--- Ejercicio12/main.cpp
+++ Ejercicio12/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 using namespace std;
 
+//Ejercicio 12. Escriba un programa que pida un número N e imprima todas las potencias desde N1 hasta N5.
+//Ej: si se ingresa 3 se debe imprimir:
+//3∧1=3
+//3∧2=9
+//3∧3=27
+//3∧4=81
+//3∧5=243
+
 int main()
 {
-    int n, total, exp=1;
+    int n, total, exp=1; //Declaramos las variables, siendo n el valor ingresado por el usuario, el total que es el producto en cada iteración, exponente que es valor que va desde 1 hasta 5.
     cout << "Ingrese un numero: ";
     cin >> n;
     for(total = 1; exp <= 5; exp++){
         total *= n;
-        cout << n <<" elevado a la " <<exp<< " = "<<total<< endl; 
-        
+        cout << n <<" elevado a la " <<exp<< " = "<<total<< endl; //Por cada iteración se imprimirá el valor
+        //de n elevado al exponente (del 1 al 5) que es total, luego en la siguiente iteración multiplicará el total
+        //nuevamente por n hasta 5, obteniendo así los resultados.
             }
 
     return 0;
